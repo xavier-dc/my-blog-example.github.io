2ee79f906addb6e31284b4ea675cf777e072d2fe
Xavier <xavier@Xaviers-Air.home>
2020-12-08 19:55:59 -0500

    First commit

---
+++ src/server.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/build")));
+
+app.use(bodyParser.json());
+
+const withDB = async (operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }); //mongodb runs on 27017 and must take the useNewUrlParser: true, it's also asyncronous so add await at beggning
+        const db = client.db('my-blog'); //now we can query the db in our mongodb database
+    
+        await operations(db);
+    
+        client.close();
+        } catch (error) {
+            res.status(500).json({ message: "Error connect to db", error});
+        }
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async(db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName })
+    res.status(200).json(articleInfo); //json is the same as .send but it works better with json objects
+    }, res)
+})
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    withDB(async(db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne({name: articleName}, {
+        '$set': {
+            upvotes: articleInfo.upvotes + 1,
+        },
+    }, );
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close(); 
+    }, res)
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    withDB(async(db) => {
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({name: articleName}, {
+            '$set': {
+                comments : articleInfo.comments.concat({ username, text} ),
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleInfo);
+    }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
+
+app.listen(8000, () => console.log("Listening on port 8000"));
